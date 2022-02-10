4b2fb35c50dc5b52b560a3ad87d0cd6b70560bae
txu3 <txu3@trip.com>
2022-02-10 21:43:29 +0800

    init

---
+++ src/one_one/demo303.java
@@ -0,0 +1,14 @@
+package one_one;
+
+public class demo303 {
+    public demo303(int[] nums) {
+
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+        }
+        return sum;
+    }
+}
