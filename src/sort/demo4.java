aeaaf6ed92a6894c0e77fe87032b5777c5c7a752
Xt6253555 <617196404@qq.com>
2022-02-12 00:19:08 +0800

    init

--- src/sort/demo4.java
+++ src/sort/demo4.java
@@ -8,24 +8,21 @@ import java.util.Arrays;
 public class demo4 {
     public static void main(String[] args) {
         int[] arr = {84, 83, 88, 87, 61, 50, 70, 60, 80, 99};
-//        int[] arr = {2,1,4,3};
         System.out.println(Arrays.toString(sort(arr)));
 
     }
     public static int[] sort(int[] arr){
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
-            for (int cur = 0; cur < gap; cur+=gap) {
-                //插入排序
-                for (int i = gap; i < arr.length; i+=gap) {
-                    int j = i;
-                    while (j>0){
-                        if(arr[j-gap]>arr[j]){
-                            int swap = arr[j-gap];
-                            arr[j-gap] = arr[j];
-                            arr[j] = swap;
-                        }
-                        j-=gap;
+            //插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i-gap;
+                while (j>=0){
+                    if(arr[j]>arr[j+gap]){
+                        int swap = arr[j+gap];
+                        arr[j+gap] = arr[j];
+                        arr[j] = swap;
                     }
+                    j-=gap;
                 }
             }
         }
