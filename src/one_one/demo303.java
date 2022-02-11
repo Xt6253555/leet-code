4322f20002081634f8cf6b7741fe05acc29cc9cf
Xt6253555 <617196404@qq.com>
2022-02-11 21:35:05 +0800

    init

--- src/one_one/demo303.java
+++ src/one_one/demo303.java
@@ -1,14 +1,17 @@
 package one_one;
 
 public class demo303 {
+    private int[] arr;
     public demo303(int[] nums) {
-
+//        构建前缀和
+        int n = nums.length;
+        arr = new int[n+1];
+        for (int i = 0; i < n; i++) {
+            arr[i+1] = nums[i]+arr[i];
+        }
     }
 
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++) {
-        }
-        return sum;
+        return arr[right+1]-arr[left];
     }
 }
