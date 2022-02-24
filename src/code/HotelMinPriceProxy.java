6f37520d6d9d87c44e103580a30c53af9efc0ae4
txu3 <txu3@trip.com>
2022-02-24 20:34:39 +0800

    init

--- src/code/HotelMinPriceProxy.java
+++ src/code/HotelMinPriceProxy.java
@@ -25,9 +25,39 @@ public class HotelMinPriceProxy {
 
     public static final int FILTER_ITEM_RELAX_SIZE = 3;
     public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next= new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        ListNode listNode = removeNthFromEnd(node, 2);
+        list(listNode);
+    }
+    //输入：head = [1,2,3,4,5], n = 2
+    //输出：[1,2,3,5]
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        // 删除倒数第 n 个，要先找倒数第 n + 1 个节点
+        ListNode x = find(dummy, n + 1);
+        // 删掉倒数第 n 个节点
+        x.next = x.next.next;
+        return dummy.next;
 
     }
-
+    public static ListNode find(ListNode head, int n){
+        ListNode p1 = head;
+        for (int i = 0; i < n; i++) {
+            p1 = p1.next;
+        }
+        ListNode p2 = head;
+        while (p1 != null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2;
+    }
     public static final int CROSS_CITY_FLAG = 1;
     public static final int NON_CROSS_CITY_FLAG = 0;
     public static final int ADD_BED_FLAG = 1;
---
+++ src/one_one/demo19.java
@@ -0,0 +1,8 @@
+package one_one;
+
+public class demo19 {
+    //删除链表的倒数第N个结点
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return head;
+    }
+}
