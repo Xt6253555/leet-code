c2740acb4f9c76fb5d96a2bdce12f697b039a065
txu3 <txu3@trip.com>
2022-02-28 20:22:35 +0800

    init

---
+++ src/one_one/demo1541.java
@@ -0,0 +1,33 @@
+package one_one;
+
+import java.util.Stack;
+
+//平衡括号字符串的最少插入次数
+public class demo1541 {
+    // 输入：s = "))())("
+    //输出：3
+    public static void main(String[] args) {
+        System.out.println(minInsertions(")("));
+    }
+    public static int minInsertions(String s) {
+        int res = 0, need = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                need += 2;
+                if (need % 2 == 1) {
+                    res++;
+                    need--;
+                }
+            }
+
+            if (c == ')') {
+                need--;
+                if (need == -1) {
+                    res++;
+                    need = 1;
+                }
+            }
+        }
+        return res+need;
+    }
+}
---
+++ src/one_one/demo678.java
@@ -0,0 +1,28 @@
+package one_one;
+
+import java.util.Stack;
+
+//有效的括号字符串
+public class demo678 {
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(check(s));
+    }
+    public static boolean check(String s){
+        int l = 0; int r = 0;
+        for(char it : s.toCharArray()) {
+            if(it == '(') {
+                l++;
+                r++;
+            } else if(it == ')') {
+                l = Math.max(l - 1,0);
+                r--;
+                if(r < 0) return false;
+            } else {
+                l = Math.max(l - 1, 0);
+                r++;
+            };
+        }
+        return l == 0;
+    }
+}
