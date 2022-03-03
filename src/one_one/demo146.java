38a9afa8c37a850724903aee298c6ce7a430ee75
txu3 <txu3@trip.com>
2022-03-03 20:45:18 +0800

    init

--- src/one_one/demo146.java
+++ src/one_one/demo146.java
@@ -1,5 +1,15 @@
 package one_one;
-
+//LRU缓存
 public class demo146 {
+    public demo146(int capacity) {
 
+    }
+
+    public int get(int key) {
+        return 1;
+    }
+
+    public void put(int key, int value) {
+        return;
+    }
 }
