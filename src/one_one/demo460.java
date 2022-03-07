b279c22f53810ed3c3dda82b8c20cc3a843b74c3
Xt6253555 <617196404@qq.com>
2022-03-07 23:02:39 +0800

    init

--- src/one_one/demo460.java
+++ src/one_one/demo460.java
@@ -25,19 +25,76 @@ public class demo460 {
     }
 
     public int get(int key) {
-        return 1;
+        if(!keyToVal.containsKey(key))return -1;
+        // 增加 key 对应的 freq
+        increaseFreq(key);
+        return keyToVal.get(key);
     }
 
     public void put(int key, int value) {
+        if (this.cap <= 0) return;
 
+        /* 若 key 已存在，修改对应的 val 即可 */
+        if (keyToVal.containsKey(key)) {
+            keyToVal.put(key, value);
+            // key 对应的 freq 加一
+            increaseFreq(key);
+            return;
+        }
+
+        /* key 不存在，需要插入 */
+        /* 容量已满的话需要淘汰一个 freq 最小的 key */
+        if (this.cap <= keyToVal.size()) {
+            removeMinFreqKey();
+        }
+
+        /* 插入 key 和 val，对应的 freq 为 1 */
+        // 插入 KV 表
+        keyToVal.put(key, value);
+        // 插入 KF 表
+        keyToFreq.put(key, 1);
+        // 插入 FK 表
+        freqToKeys.putIfAbsent(1, new LinkedHashSet<>());
+        freqToKeys.get(1).add(key);
+        // 插入新 key 后最小的 freq 肯定是 1
+        this.minFreq = 1;
     }
 
     //次数加1(更新kf，fk)
     private void increaseFreq(int key) {
-
+        int freq = keyToFreq.get(key);
+        /* 更新 KF 表 */
+        keyToFreq.put(key, freq + 1);
+        /* 更新 FK 表 */
+        // 将 key 从 freq 对应的列表中删除
+        freqToKeys.get(freq).remove(key);
+        // 将 key 加入 freq + 1 对应的列表中
+        freqToKeys.putIfAbsent(freq + 1, new LinkedHashSet<>());
+        freqToKeys.get(freq + 1).add(key);
+        // 如果 freq 对应的列表空了，移除这个 freq
+        if (freqToKeys.get(freq).isEmpty()) {
+            freqToKeys.remove(freq);
+            // 如果这个 freq 恰好是 minFreq，更新 minFreq
+            if (freq == this.minFreq) {
+                this.minFreq++;
+            }
+        }
     }
     //移除最少使用的key
     private void removeMinFreqKey() {
-
+        // freq 最小的 key 列表
+        LinkedHashSet<Integer> keyList = freqToKeys.get(this.minFreq);
+        // 其中最先被插入的那个 key 就是该被淘汰的 key
+        int deletedKey = keyList.iterator().next();
+        /* 更新 FK 表 */
+        keyList.remove(deletedKey);
+        if (keyList.isEmpty()) {
+            freqToKeys.remove(this.minFreq);
+            // 问：这里需要更新 minFreq 的值吗？
+        }
+        /* 更新 KV 表 */
+        keyToVal.remove(deletedKey);
+        /* 更新 KF 表 */
+        keyToFreq.remove(deletedKey);
     }
 }
