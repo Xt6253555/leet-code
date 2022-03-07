9167ee6800e009d10ad63744374e5ff0df29ba77
Xt6253555 <617196404@qq.com>
2022-03-07 23:39:23 +0800

    init

--- src/one_one/demo142.java
+++ src/one_one/demo142.java
@@ -1,4 +1,18 @@
 package one_one;
+
+import java.util.HashSet;
+
 //环形链表2
 public class demo142 {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head!=null){
+            if(set.contains(head.next)){
+                return head.next;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return null;
+    }
 }
