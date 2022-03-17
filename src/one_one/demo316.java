7ef058e984e8e4a0a163ef3fb6458dde48ca5009
Xt6253555 <617196404@qq.com>
2022-03-17 22:58:22 +0800

    init

--- src/one_one/demo316.java
+++ src/one_one/demo316.java
@@ -1,6 +1,5 @@
 package one_one;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 //去除重复字母
@@ -17,13 +16,15 @@ public class demo316 {
         boolean[] flag = new boolean[256];
         for (char c : s.toCharArray()) {
             count[c]--;
+            //判断栈里面有类似元素直接返回
             if(flag[c])continue;
             while (!stack.isEmpty()&&stack.peek()>c){
+                //判断后面无相同元素，返回
                 if(count[stack.peek()]==0){
                     break;
-                }else{
-                    flag[stack.pop()] = false;
                 }
+                //栈弹出的元素置为false，可再次入栈
+                flag[stack.pop()] = false;
             }
             stack.push(c);
             flag[c]=true;
