1c8da68d74348937d1932c1c53853e2ba69213a7
txu3 <txu3@trip.com>
2022-04-12 22:15:42 +0800

    init

---
+++ src/two_one/TreeNode.java
@@ -0,0 +1,26 @@
+package two_one;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    public static void pre(TreeNode root){
+        if(root==null)return;
+        System.out.println(root.val);
+        pre(root.left);
+        pre(root.right);
+    }
+}
---
+++ src/two_one/demo226.java
@@ -0,0 +1,24 @@
+package two_one;
+//翻转二叉树
+public class demo226 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(7);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        TreeNode treeNode = invertTree(root);
+        TreeNode.pre(root);
+    }
+    public static TreeNode invertTree(TreeNode root) {
+        if(root==null)return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
