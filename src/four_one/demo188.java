a51bfaa561a626805eb8a5583e5fea6d0881f908
txu3 <txu3@trip.com>
2022-04-29 20:21:29 +0800

    init

--- src/four_one/demo188.java
+++ src/four_one/demo188.java
@@ -1,7 +1,23 @@
 package four_one;
 //买卖股票的最佳时机 IV
 public class demo188 {
-    public int maxProfit(int k, int[] prices) {
-
+    public static void main(String[] args) {
+        int[] arr = {2,4,1};
+        int i = maxProfit(2, arr);
+        System.out.println(i);
+    }
+    public static int maxProfit(int k, int[] prices) {
+        if(k==0||prices.length<=1)return 1;
+        int[][] dp= new int[2][k+1];
+        for (int j = 1; j <= k; j++) {
+            dp[1][j] = -prices[0];
+        }
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[0][j]= Math.max(dp[0][j],dp[1][j]+prices[i]);
+                dp[1][j] = Math.max(dp[1][j],dp[0][j-1]-prices[i]);
+            }
+        }
+        return dp[0][k];
     }
 }
--- src/four_one/demo309.java
+++ src/four_one/demo309.java
@@ -1,4 +1,16 @@
 package four_one;
 //最佳买卖股票时机含冷冻期
 public class demo309 {
+    public int maxProfit(int[] prices) {
+        int dp = 0;
+        int pre = 0;
+        int min = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            int temp = dp;
+            dp = Math.max(dp,dp+min);
+            min =Math.max(min,pre-prices[i]);
+            pre = temp;
+        }
+        return dp;
+    }
 }
