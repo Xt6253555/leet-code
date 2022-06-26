ac533f4647b959f794201c63ce065a91b89dafcc
Xt6253555 <617196404@qq.com>
2022-06-27 00:13:45 +0800

    init

--- src/sort/test.java
+++ src/sort/test.java
@@ -1,42 +1,7 @@
 package sort;
 
-import java.util.Arrays;
-
 public class test {
     public static void main(String[] args) {
-        int[]arr = {4,5,1,2,6};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
 
-    }
-    public static void sort(int[] arr){
-        // 建立长度为 9 的数组，下标 0~8 对应数字 1~9
-        int[] counting = new int[6];
-        // 遍历 arr 中的每个元素
-        for (int element : arr) {
-            // 将每个整数出现的次数统计到计数数组中对应下标的位置
-            counting[element - 1]++;
-        }
-        // 记录前面比自己小的数字的总数
-        int preCounts = 0;
-        for (int i = 0; i < counting.length; i++) {
-            int temp = counting[i];
-            // 将 counting 计算成当前数字在结果中的起始下标位置。位置 = 前面比自己小的数字的总数。
-            counting[i] = preCounts;
-            // 当前的数字比下一个数字小，累计到 preCounts 中
-            preCounts += temp;
-        }
-        int[] result = new int[arr.length];
-        for (int element : arr) {
-            // counting[element - 1] 表示此元素在结果数组中的下标
-            int index = counting[element - 1];
-            result[index] = element;
-            // 更新 counting[element - 1]，指向此元素的下一个下标
-            counting[element - 1]++;
-        }
-        // 将结果赋值回 arr
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = result[i];
-        }
     }
 }
---
+++ src/three_one/demo22.java
@@ -0,0 +1,28 @@
+package three_one;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//括号生成
+public class demo22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if(n==0)return res;
+        dfs("", 0, 0, n, res);
+        return res;
+    }
+
+    private void dfs(String s, int left, int right, int n, List<String> res) {
+        if(left==n&&right==n){
+            res.add(s);
+            return;
+        }
+        if(left<right)return;
+        if(left<n){
+            dfs(s+"(",left+1,right,n,res);
+        }
+        if(right<n){
+            dfs(s+")",left,right+1,n,res);
+        }
+    }
+}
